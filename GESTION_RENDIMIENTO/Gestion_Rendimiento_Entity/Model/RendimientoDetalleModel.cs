a0e24e424f5a21a005468a5e3db6258515b9c55a
MIGUEL MORALES <miguel_morales_k@hotmail.com>
2023-07-15 22:49:42 -0500

    15072023_V3

---
+++ GESTION_RENDIMIENTO/Gestion_Rendimiento_Entity/Model/RendimientoDetalleModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gestion_Rendimiento_Entity.Model
+{
+    public class RendimientoDetalleModel
+    {
+        public int ID_PROYECTO { get; set; }
+        public int ID_DETALLE { get; set; }
+
+        public string EVIDENCIA { get; set; }
+        public string PLAZOS { get; set; }
+        public string INDICADOR { get; set; }
+        public int VALOR { get; set; }
+    }
+}
---
+++ GESTION_RENDIMIENTO/Gestion_Rendimiento_Entity/Model/RendimientoDetalleSubModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gestion_Rendimiento_Entity.Model
+{
+    public class RendimientoDetalleSubModel
+    {
+        public int ID_DETALLE { get; set; }
+        public int ID_DETALLE_SUB { get; set; }
+        
+        public string EVIDENCIA  { get; set; }
+        public string PLAZOS { get; set; }
+        public string INDICADOR { get; set; }
+        public int VALOR { get; set; }
+    }
+}
